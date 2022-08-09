e2427abc2b5029e7a5f6049e72dd02502871aef4
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-08-09 15:40:53 -0300

    att o banco

--- Banco/fisico.sql
+++ Banco/fisico.sql
@@ -2,6 +2,12 @@ create database pokedex;
 
 use pokedex;
 
+create table categoria (
+	id int auto_increment not null,
+    descricao varchar(250) not null,
+    primary key (id)
+);
+
 create table pokemon (
 	id int auto_increment not null,
     nome varchar(250) not null,
@@ -13,9 +19,5 @@ create table pokemon (
     foreign key (id_categoria) references categoria(id)
 );
 
-create table categoria (
-	id int auto_increment not null,
-    descricao varchar(250) not null,
-    primary key (id)
-);
+
     
