09f9b8f11524ef6c79a5917a7f0d1890c8536423
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-08-15 15:33:10 -0300

    att rotas

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 $uri_parse = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 include 'Controller/PokemonController.php';
+include 'Controller/CategoriaController.php';
 
 switch($uri_parse)
 {
@@ -20,6 +21,24 @@ switch($uri_parse)
 
     case '/pokemon/delete':
         PokemonController::delete();
+    break;
+
+
+
+    case '/categoria':
+        CategoriaController::index();
+    break;
+
+    case '/categoria/form':
+        CategoriaController::form();
+    break;
+
+    case '/categoria/save':
+        CategoriaController::save();
+    break;
+
+    case 'categoria/delete':
+        CategoriaController::delete();
     break;
 
 
