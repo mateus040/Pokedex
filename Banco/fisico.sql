278176045525bb8c673ade4f71ee8346233a48e2
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-08-16 15:58:08 -0300

    alterando dados na tabela pokemon

--- Banco/fisico.sql
+++ Banco/fisico.sql
@@ -7,7 +7,7 @@ create table pokemon (
     nome varchar(250) not null,
     descricao varchar(250) not null,
     tipo varchar(250) not null,
-    imagem longblob not null,
+    imagem longblob,
     primary key(id)
 );
     
