430b613006700bf6477f470e31692fea111aa724
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-08-17 14:11:52 -0300

    atualizando forms e list

--- View/modules/Pokemon/FormPokemon.php
+++ View/modules/Pokemon/FormPokemon.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Pokemons</title>
     <link rel="stylesheet" href="/../../css/Form.css">
+    <link rel="shortcut icon" href="/../../img/favicon.png" type="image/x-icon">
     <style>
         label, input { display: block;}
         body{
