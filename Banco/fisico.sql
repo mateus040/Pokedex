9c33fe59a58bdc3e86dc5784497f614137685313
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-08-18 22:38:44 -0300

    removendo o campo tipo

--- Banco/fisico.sql
+++ Banco/fisico.sql
@@ -2,13 +2,20 @@ create schema pokedex;
 
 use pokedex;
 
+create table categoria (
+	id int auto_increment not null,
+    descricao varchar(250) not null,
+    primary key(id)
+);
+
 create table pokemon (
 	id int auto_increment not null,
     nome varchar(250) not null,
     descricao varchar(250) not null,
-    tipo varchar(250) not null,
     imagem varchar(500) not null,
-    primary key(id)
+    id_categoria int,
+    primary key(id),
+	foreign key (id_categoria) references categoria(id)
 );
     
     
