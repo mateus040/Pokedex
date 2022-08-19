1d051f531cca7af700ab7bba3dbc401ca22a3b6e
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-08-19 15:41:21 -0300

    adicionando links

--- View/includes/cabecalho.php
+++ View/includes/cabecalho.php
@@ -1,3 +1,8 @@
+<head>
+    <link rel="stylesheet" href="/../css/style_cabecalho.css">
+</head>
+
+
 <nav>
 
     <img src="/../img/logo.png">
@@ -7,6 +12,8 @@
         <li> <a href="/home">Home</a></li>
         <li> <a href="/pokemon/form">Cadastrar Pokémon</a></li>
         <li> <a href="/pokemon">Listagem de Pokémon</a></li>
+        <li> <a href="/categoria/form">Cadastrar Categoria</a></li>
+        <li> <a href="/categoria">Listagem de Categoria</a></li>
 
 
     </ul>
