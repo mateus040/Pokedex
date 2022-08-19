8979faea169e536d29ae0542e25097d9bbb5c7de
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-08-19 15:41:38 -0300

    adicionando include e links

--- View/modules/Categoria/FormCategoria.php
+++ View/modules/Categoria/FormCategoria.php
@@ -4,29 +4,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastrar Categoria de Pokémon</title>
+    <title>Cadastro de Pokemons</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="/../../css/style_FormCategoria.css">
+    <link rel="shortcut icon" href="/../../img/favicon.png" type="image/x-icon">
     <style>
-        input, label {display: block}
+        label, input { display: block;}
+        body{
+        font-family: Arial, Helvetica, sans-serif;
+    }
     </style>
 </head>
 <body>
 
     <form action="/categoria/save" method="post">
 
-        <fieldset>
+        <header>
+            <?php include 'View/includes/cabecalho.php' ?>
+        </header>
 
-            <legend>Cadastro de Categoria de Pokémon</legend>
+        <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+
+        <div class="container">
+
+            <h2>Cadastro de Categoria</h2>
 
             <input type="hidden" value="<?= $model->id ?>" name="id" />
 
             <label for="descricao">Descrição:</label>
             <input type="text" name="descricao" id="descricao" value="<?= $model->descricao ?>" />
 
-            <br>
-
             <button type="submit">Cadastrar</button>
 
-        </fieldset>
+        </div>
+
+
 
     </form>
     
