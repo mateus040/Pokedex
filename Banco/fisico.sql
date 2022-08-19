f145bff3d00a70cc64eb1ee0f8fe13d91b356098
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-08-19 16:38:13 -0300

    atualizando banco

--- Banco/fisico.sql
+++ Banco/fisico.sql
@@ -19,15 +19,5 @@ create table pokemon (
 	foreign key (id_categoria) references categoria(id)
 );
 
-select * from categoria;
-select * from pokemon;
-
-select p.id, p.nome, p.descricao, c.descricao as categoria, p.imagem
-from pokemon p
-join categoria c on (c.id = p.id_categoria);
-
-insert into categoria (descricao) value ("terra");    
-
-insert into pokemon (id_categoria, nome, descricao, imagem) values (3, "picachu", "correr", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png"); 
 
     
