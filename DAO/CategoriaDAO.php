3caa3f61c7c5ae3e4fe7ee897ca8c45e8b3ac6c6
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-08-19 14:56:56 -0300

    adicionando DAO de categoria

---
+++ DAO/CategoriaDAO.php
@@ -0,0 +1,62 @@
+<?php
+
+class CategoriaDAO
+{
+    private $conexao;
+
+    public function __construct()
+    {
+        include_once 'MySQL.php';
+
+        $this->conexao = new MySQL();
+    }
+
+    public function insert(CategoriaModel $model)
+    {
+        $sql = 'INSERT INTO categoria (descricao) VALUES (?)';
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $model->descricao);
+        $stmt->execute();
+    }
+
+    public function update(CategoriaModel $model)
+    {
+        $sql = 'UPDATE categoria SET descricao=? WHERE id=?';
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $model->descricao);
+        $stmt->bindValue(2, $model->id);
+        $stmt->execute();
+    }
+
+    public function select()
+    {
+        $sql = 'SELECT * FROM categoria';
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->execute();
+    }
+
+    public function selectById(int $id)
+    {
+        include_once 'Model/CategoriaModel.php';
+
+        $sql = 'SELECT * FROM categoria WHERE id=?';
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $model->id);
+        $stmt->execute();
+
+        return $stmt->fetchObject("CategoriaModel");
+    }
+
+    public function delete(int $id)
+    {
+        $sql = 'DELETE * FROM categoria WHERE id=?';
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $id);
+        $stmt->execute();
+    }
+}
